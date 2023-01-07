41aee8498daf1e410e4b27474ce5c2673101e2b2
Steph Hjartarson <stephanie.hja@gmail.com>
2023-01-07 18:46:31 +0000

    Render list of posts

---
+++ my-app/src/components/Content.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { savedPosts } from '../posts.json'
+import css from './css/Content.module.css'
+import PostItem from './PostItem'
+
+export class Content extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className={css.Content}>
+
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                </div>
+
+                <div className={css.SearchResults}>
+                    <PostItem posts={savedPosts} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Content
